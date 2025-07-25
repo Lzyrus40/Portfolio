b1e1eacd565303a741c461a9db926bad5117070d
Lzyrus <lzyrus40@gmail.com>
2025-07-25 17:51:03 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || '/react-vite-deploy',
+  base: process.env.VITE_BASE_PATH || "/react—vite—deploy",
 })
